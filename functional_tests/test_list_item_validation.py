c8041c9e21a8052cdbcffb4fbe20df6de210adbb
mattkohl <hlaford@gmail.com>
2017-07-23 15:07:21 +0100

    split tests into multiple files

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,5 @@
 from unittest import skip
-
+from selenium.webdriver.common.keys import Keys
 from functional_tests.base import FunctionalTest
 
 
@@ -7,4 +7,31 @@ class ItemValidationTest(FunctionalTest):
 
     @skip
     def test_cannot_add_empty_list_items(self):
-        pass
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+
+        self.assertEqual(
+            self.browser.find_element_by_css_selector(".has-error").text,  
+            "You can't have an empty list item"  
+        )
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
+        
+        self.browser.find_element_by_id("id_new_item").send_keys("Dada")
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Dada")
+
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector(".has-error").text,
+            "You cannot have an empty list item"
+        ))
+
+        self.browser.find_element_by_id("id_new_item").send_keys("Mama")
+        self.browser.find_element_by_id("id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Dada")
+        self.wait_for_row_in_list_table("2: Mama")
