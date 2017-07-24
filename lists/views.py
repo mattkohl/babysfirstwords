4af3c2b8740b2ba3c9d62246170368ccc3f8f60d
mattkohl <hlaford@gmail.com>
2017-07-24 07:39:10 +0100

    now using reverse url lookup

--- lists/views.py
+++ lists/views.py
@@ -15,7 +15,7 @@ def new_list(request):
     except ValidationError:
         error = "You cannot have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect("/lists/{}/".format(list_.id))
+    return redirect(list_)
 
 
 def view_list(request, list_id):
@@ -27,7 +27,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST["item_text"], list=list_)
             item.full_clean()
             item.save()
-            return redirect("/lists/{}/".format(list_.id))
+            return redirect(list_)
         except ValidationError:
             error = "You cannot have an empty list item"
             
