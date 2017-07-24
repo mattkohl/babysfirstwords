e1de3ec763bf9b5a035adc76c1e83ff147604c7a
mattkohl <hlaford@gmail.com>
2017-07-24 18:53:52 +0100

    updates to model tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -35,3 +35,17 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Dada")
         self.wait_for_row_in_list_table("2: Mama")
+        
+    def test_cannot_add_duplicate_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys("Dada")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Dada")
+    
+        self.get_item_input_box().send_keys("Dada")
+        self.get_item_input_box().send_keys(Keys.ENTER)
+    
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector(".has-error").text,
+            "You already have this in your list"
+        ))
--- lists/models.py
+++ lists/models.py
@@ -7,8 +7,17 @@ class List(models.Model):
     def get_absolute_url(self):
         return reverse("view_list", args=[self.id])
 
+    def __str__(self):
+        return str(self.id)
+
 
 class Item(models.Model):
     text = models.TextField(default="")
     list = models.ForeignKey(List, default=None)
 
+    class Meta:
+        ordering = ('id',)
+        unique_together = ("list", "text")
+
+    def __str__(self):
+        return self.text
