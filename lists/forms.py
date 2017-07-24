254b2270f9871ca6ea362a7eac681a7d3991ba73
mattkohl <hlaford@gmail.com>
2017-07-24 07:49:51 +0100

    added form

---
+++ lists/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from lists.models import Item
+
+
+EMPTY_ITEM_ERROR = "You cannot have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ("text",)
+        widgets = {
+            "text": forms.fields.TextInput(attrs={
+                "placeholder": "Enter a word",
+                "class": "form-control pure-input-1",
+            }),
+        }
+        error_messages = {
+            "text": {"required": EMPTY_ITEM_ERROR}
+        }
