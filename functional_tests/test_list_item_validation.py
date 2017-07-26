e8c90f032ec30207ff04202aea971842d4f58c6e
mattkohl <hlaford@gmail.com>
2017-07-26 06:06:19 +0100

    removed ft skip

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -36,7 +36,6 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table("1: Dada")
         self.wait_for_row_in_list_table("2: Mama")
 
-    @skip
     def test_cannot_add_duplicate_items(self):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys("Dada")
