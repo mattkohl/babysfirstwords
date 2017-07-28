6b7c4ffb49b1bd97fb150f522c7e7555be1509dd
mattkohl <hlaford@gmail.com>
2017-07-28 19:16:47 +0100

    more progress on auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,21 @@
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.core.urlresolvers import reverse
+from accounts.models import Token
 
 
 def send_login_email(request):
     email = request.POST["email"]
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse("login") + "?token=" + str(token.uid)
+    )
+    message_body = "Use this link to log in:\n\n{}".format(url)
+
     send_mail(
         "Your login link for Baby's First Words",
-        "body text tbc",
+        message_body,
         "noreply@babys-first-words",
         [email],
     )
