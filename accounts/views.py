529cd6348f479b54a95540df85cb74a075802eec
mattkohl <hlaford@gmail.com>
2017-07-28 18:58:59 +0100

    still working on auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def send_login_email(request):
+    email = request.POST["email"]
+    send_mail(
+        "Your login link for Baby's First Words",
+        "body text tbc",
+        "noreply@babys-first-words",
+        [email],
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect("/")
+
+
+def login(request):
+    return redirect("/")
